92f51b978046661e95ffa29223e8d908fa168637
Timbait <justlookfortim@gmail.com>
2024-02-23 17:34:28 +0500

    requirements.txt

--- devices/admin.py
+++ devices/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-from .models import DeviceType, DeviceModel, Parameter, Device
+from .models import Device, DeviceType, DeviceModel
+from django.utils.translation import gettext as _
+from django.contrib import messages
 
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_type', 'device_model')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "device_model":
+            device_type_id = request.POST.get('device_type') or \
+                             kwargs.get('initial', {}).get('device_type_id')  # Получаем device_type_id из начальных данных
+            if device_type_id:
+                kwargs["queryset"] = DeviceModel.objects.filter(device_type_id=device_type_id)
+            else:
+                kwargs["queryset"] = DeviceModel.objects.none()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if obj:  # Если устройство уже существует (редактирование)
+            form.base_fields['device_model'].queryset = DeviceModel.objects.filter(device_type=obj.device_type)
+        return form
+
+    def message_user(self, request, message, level=messages.INFO, extra_tags='', fail_silently=False):
+        message = _('Устройство создано !')
+        return super().message_user(request, message, level=level, extra_tags=extra_tags, fail_silently=fail_silently)
+
+
+admin.site.register(Device, DeviceAdmin)
 admin.site.register(DeviceType)
 admin.site.register(DeviceModel)
-admin.site.register(Parameter)
-
-@admin.register(Device)
-class DeviceAdmin(admin.ModelAdmin):
-    filter_horizontal = ('parameters',)
--- devices/models.py
+++ devices/models.py
@@ -1,25 +1,15 @@
 from django.db import models
-from django.utils import timezone
-
 
 class DeviceType(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class DeviceModel(models.Model):
-    name = models.CharField(max_length=50)
-    devicetype = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Parameter(models.Model):
-    name = models.CharField(max_length=50)
-    devicetype = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -28,8 +18,11 @@ class Parameter(models.Model):
 class Device(models.Model):
     device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
     device_model = models.ForeignKey(DeviceModel, on_delete=models.CASCADE)
-    parameters = models.ManyToManyField(Parameter)
 
     def __str__(self):
-        return f"Устройство {self.pk}"
+        return str(self.pk)
+
+
+
+
 
