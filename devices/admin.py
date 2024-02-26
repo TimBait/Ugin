323f6b5f72dbcc945b402fe2c9ed7e01ae180ddd
Timbait <justlookfortim@gmail.com>
2024-02-26 07:00:30 +0500

    requirements.txt

--- devices/admin.py
+++ devices/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin, messages
 from .models import Device, DeviceType, DeviceModel, Parameter
 
 
-
 class ParameterInline(admin.StackedInline):
     model = Parameter
     extra = 1
@@ -18,7 +17,7 @@ class DeviceAdmin(admin.ModelAdmin):
         if db_field.name == "device_model":
             device_type_id = request.POST.get('device_type') or \
                              kwargs.get('initial', {}).get(
-                                 'device_type_id')  
+                                 'device_type_id')
             if device_type_id:
                 kwargs["queryset"] = DeviceModel.objects.filter(device_type_id=device_type_id)
             else:
@@ -32,7 +31,7 @@ class DeviceAdmin(admin.ModelAdmin):
         return form
 
     def message_user(self, request, message, level=messages.INFO, extra_tags='', fail_silently=False):
-        message = ('Устройство создано !')
+        message = 'Устройство создано !'
         return super().message_user(request, message, level=level, extra_tags=extra_tags, fail_silently=fail_silently)
 
 
@@ -48,4 +47,3 @@ class ParameterAdmin(admin.ModelAdmin):
 
 
 admin.site.register(DeviceType)
-
